cbacd85cc2a97acb99a8703c29d760fcaf795779
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-26 12:32:32 +0100

    finished fetchsearch fuctio ad render on the results page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,10 +1,32 @@
+// import fetchSearch from "@/lib/fetchSearch";
+
+import { fetchSearch } from "@/lib/fetchSearch";
+
 type Props = {
   searchParams: {
     q: string;
   };
 };
-function SearchPage({ searchParams: { q } }: Props) {
-  return <div>SearchPage</div>;
+async function SearchPage({ searchParams: { q } }: Props) {
+  //Fetch the search results
+  const results = await fetchSearch(q);
+  // console.log(results);
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Results for {q} </h1>
+      <h2 className="mb-5 text-gray-400">
+        ({results?.content.total_results} results)
+      </h2>
+
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {results?.content.organic.map((product) => (
+          <li key={product.product_id}>
+            <p>{product.title}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default SearchPage;
