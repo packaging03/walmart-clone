f5f82361ae88e5956a67e2bf5e9314f39b83d629
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-27 08:56:33 +0100

    completed products component

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,11 +1,15 @@
 import { Organic } from "@/lib/interfaces";
+import { Badge } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 function Product({ product }: { product: Organic }) {
   return (
-    <Link href={{ pathname: "/product", query: { url: product.url } }}>
+    <Link
+      href={{ pathname: "/product", query: { url: product.url } }}
+      className="flex flex-col relative border rounded-md h-full p-5"
+    >
       <Image
         src={product.image}
         alt={product.title}
@@ -13,10 +17,24 @@ function Product({ product }: { product: Organic }) {
         height={200}
         className="mx-auto"
       />
+
       <p className="text-xl font-bold">
         {product.price?.currency}
         {product.price.price}
       </p>
+
+      {product.badge && (
+        <Badge className="w-fit absolute top-2 right-2">{product.badge}</Badge>
+      )}
+
+      <p className="font-light">{product.title}</p>
+
+      {product.rating && (
+        <p className="text-yellow-500 text-sm">
+          {product.rating.rating} ✭
+          <span className="text-gray-400 ml-2">({product.rating.count})</span>
+        </p>
+      )}
     </Link>
   );
 }
