3d1312003327a29e2dec9fe24fdc6c2155ab24d9
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-28 19:16:26 +0100

    add fetchproduct function

---
+++ lib/fetchProduct.ts
@@ -0,0 +1,42 @@
+import { ProductContent } from "./interfaces";
+
+async function fetchProduct(url: string) {
+  const username = process.env.OXYLABS_USERNAME;
+  const password = process.env.OXYLABS_PASSWORD;
+
+  const newUrl = new URL(`https://www.walmart.com${url}`);
+
+  console.log("Scraping >>> ", newUrl.toString());
+
+  const body = {
+    source: "universal_ecommerce",
+    url: newUrl.toString(),
+    geo_location: "United States",
+    parse: true,
+  };
+
+  const response = fetch("https://realtime.oxylabs.io/v1/queries", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
+    },
+    next: {
+      revalidate: 60 * 60 * 24, // NOTE: ISR for 1 day
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.results.length === 0) return;
+      const result: ProductContent = data.results[0];
+      const product = result.content;
+      return product;
+    })
+    .catch((err) => console.error(err));
+
+  return response;
+}
+
+export default fetchProduct;
