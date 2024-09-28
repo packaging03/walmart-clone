cea20631866b265081241c912ec872e0cb2e9979
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-28 19:18:10 +0100

    added productpage, loading animation and the interfaces

---
+++ app/product/page.tsx
@@ -0,0 +1,106 @@
+import fetchProduct from "@/lib/fetchProduct";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Badge } from "@/components/ui/badge";
+
+type Props = {
+  searchParamas: {
+    url: string;
+  };
+};
+
+async function ProductPage({ searchParamas: { url } }: Props) {
+  const product = await fetchProduct(url);
+  if (!product) return notFound();
+
+  return (
+    <div className="p-4 lg:p-10 flex flex-col lg:flex-row w-full">
+      <div className="hidden lg:inline space-y-4">
+        {product.images.map((image, i) => (
+          <Image
+            key={image}
+            src={image}
+            alt={product.title + " " + i}
+            width={90}
+            height={90}
+            className="border rounded-sm"
+          />
+        ))}
+      </div>
+
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        className="w-3/5 mb-10 lg:mb-0 lg:w-full self-start flex items-center max-w-xl mx-auto lg:mx-20"
+      >
+        <CarouselContent>
+          {product.images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <div className="flex aspect-square items-center justify-center p-2 relative">
+                  <Image
+                    key={image}
+                    src={image}
+                    alt={product.title + " " + index}
+                    width={400}
+                    height={400}
+                  />
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+      <div className="flex-1 border rounded-md w-full p-5 space-y-5">
+        <h1 className="text-3xl font-bold">{product.title}</h1>
+        <div className="space-x-2">
+          {product.breadcrumbs.map((breadcrumb, index) => (
+            <Badge
+              key={breadcrumb + index}
+              className={breadcrumb}
+              variant="outline"
+            >
+              {breadcrumb}
+            </Badge>
+          ))}
+        </div>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          className="py-5"
+        />
+        {product.rating && (
+          <p className="text-yellow-500 text-sm">
+            {product.rating.rating} ✭
+            <span className="text-gray-400 ml-2">
+              ({product.rating.count} reviews)
+            </span>
+          </p>
+        )}
+
+        <p className="text-2xl font-bold mt-2">
+          {product?.currenncy} {product.price}
+        </p>
+
+        {/* Add to Cart Bu */}
+        <hr/>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -99,3 +99,105 @@ export interface Job {
   client_notes: null | string;
   _links: Link[];
 }
+
+export interface WalmartProductResult {
+  results: ProductContent[];
+  job: JobDetails;
+}
+
+export interface ProductContent {
+  content: Product;
+  created_at: string;
+  updated_at: string;
+  page: number;
+  url: string;
+  job_id: string;
+  status_code: number;
+  parser_type: string;
+}
+export interface Product {
+  url: string;
+  meta: Meta;
+  price: number;
+  title: string;
+  images: string[];
+  rating: Rating; //
+  seller: Seller;
+  currenncy: string;
+  warranty: string;
+  _warnings: string[];
+  variations: Variation[];
+  breadcrumbs: string[];
+  description: string;
+  out_of_stock: boolean;
+  specifications: Specification[];
+  parse_status_code: number;
+  price_strikethrough?: number;
+}
+
+export interface Meta {
+  sku: string;
+  gtin: string;
+}
+
+export interface Seller {
+  id: string;
+  url: string;
+  name: string;
+  catalog_id: string;
+  official_name: string;
+}
+
+export interface Variation {
+  state: string;
+  product_id: string;
+  selected_options: SelectedOption[];
+}
+
+export interface SelectedOption {
+  key: string;
+  value: string;
+}
+
+export interface Specification {
+  key: string;
+  value: string;
+}
+
+export interface JobDetails {
+  callback_url: string;
+  client_id: string;
+  context: JobContext[];
+  created_at: string;
+  domain: string;
+  geo_location: null | string;
+  id: string;
+  limit: number;
+  locale: null | string;
+  pages: number;
+  parse: boolean;
+  parser_type: null | string;
+  parsing_instructions: null | string;
+  browser_instructions: null | string;
+  render: null | boolean;
+  url: string;
+  query: string;
+  source: string;
+  start_page: number;
+  status: string;
+  storage_type: null | string;
+  storage_url: null | string;
+  subdomain: string;
+  content_encoding: string;
+  updated_at: string;
+  user_agent_type: string;
+  session_info: null | string;
+  statuses: unknown[];
+  client_notes: null | string;
+  _links: Link[];
+}
+
+export interface JobContext {
+  key: string;
+  value: unknown;
+}
