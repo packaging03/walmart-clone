bfc017b414fad360f784579b1180f8607141e77e
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-29 11:53:36 +0100

    getCartTotal added

---
+++ lib/getCartTotal.ts
@@ -0,0 +1,11 @@
+import { Product } from "./interfaces";
+
+export function getCartTotal(products: Product[]): string {
+  const total = products.reduce(
+    (accumulator: number, currentProduct: Product) =>
+      accumulator + currentProduct.price,
+    0
+  );
+
+  return `${products[0]?.currenncy} ${total.toFixed(2)}}`;
+}
